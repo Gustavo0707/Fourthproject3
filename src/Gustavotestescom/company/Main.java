1a904990ef67f8eeb9b5c16305512469565b0f33
Gustavo0707 <gustavomelo0703@gmail.com>
2021-05-31 16:34:12 -0300

    Initial commit

---
+++ src/Gustavotestescom/company/Main.java
@@ -0,0 +1,21 @@
+package Gustavotestescom.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char[] vet = {'a','b','c', 'd', 'e','f','g','h','i','j'};
+        int count = 0;
+
+
+        for(int i=0; i < vet.length; i++){
+            if((vet[i] != 'a') && (vet[i] != 'e') && (vet[i] != 'i') &&
+                    (vet[i] != 'o') && (vet[i] != 'u')){
+                count = count + 1;
+                System.out.println(vet[i]);
+            }
+        }
+
+        System.out.print("Número de Consoantes: ");
+        System.out.print(count);
+    }
+}
